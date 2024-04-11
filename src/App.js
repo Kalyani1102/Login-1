7b916aba69f3c7d49ef01f25d9f139f87d9e5b3c
kalyani1102 <anut4660@gmail.com>
2024-04-11 22:40:26 +0530

    from new branch

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <form>
+      <h1>Login Form</h1>
+      <div className='ui ndivider'></div>
+      <div className='ui form'></div>
+      <div className='field'>
+        <label>Username</label>
+      </div>
+     </form>
     </div>
   );
 }
